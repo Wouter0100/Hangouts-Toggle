4f7e73c024e6aabd5d8df9ac004f0e3a3f1140fa
Wouter van Os <wouter@wouter0100.nl>
2015-03-20 18:07:43 +0100

    Added a check to ensure both (mic and webcam) are in the same state.

--- assets/js/update.js
+++ assets/js/update.js
@@ -1,10 +1,11 @@
+var muted = false;
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request == 'toggle') {
-        var microphoneButton = document.querySelector('.ha-w-P-y-Xi-f');
-        simulateClick(microphoneButton);
+        muted = !muted;
 
-        var webcamButton = document.querySelector('.ha-w-P-y-si-f');
-        simulateClick(webcamButton);
+        updateButton('.ha-w-P-y-Xi-f'); // Microphone
+        updateButton('.ha-w-P-y-si-f'); // Webcam
 
         return true;
     }
@@ -12,7 +13,26 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     return false;
 });
 
-var simulateClick = function (element) {
+function updateButton(selector) {
+    // If muted == true AND hangouts muted == true, do nothing.
+    // If muted == false AND hangouts muted == false, do nothing.
+
+    var button = document.querySelector(selector);
+
+    // If muted == false AND hangouts muted == true, simulate click in Hangouts.
+    if (button.parentElement.getAttribute('aria-pressed') == 'false' && muted) {
+        simulateClick(button);
+    }
+
+    button = document.querySelector(selector); //Reselect to ensure there is a element which is valid, since we may have clicked simulateClick.
+
+    // If muted == true AND hangouts muted == false, simulate click in Hangouts.
+    if (button.parentElement.getAttribute('aria-pressed') == 'true' && !muted) {
+        simulateClick(button);
+    }
+}
+
+function simulateClick (element) {
     var initEvent = function (elt, str) {
         var clickEvent = document.createEvent('MouseEvents');
         clickEvent.initEvent(str, true, true);
@@ -21,4 +41,4 @@ var simulateClick = function (element) {
     initEvent(element, 'mousedown');
     initEvent(element, 'click');
     initEvent(element, 'mouseup');
-};
+}
