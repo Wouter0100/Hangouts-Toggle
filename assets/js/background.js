232be1764c74c15f5c17e3185b4ec712b1e85e36
Wouter van Os <wouter0100@gmail.com>
2016-01-31 14:16:02 +0100

    Fixed Hangouts Beta, implemented microphone and webcam toggle
    This fixes #3, #2 and #1.

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,9 +1,14 @@
 chrome.commands.onCommand.addListener(function(command) {
-    chrome.tabs.query({ url: 'https://plus.google.com/hangouts/*' }, function(tabs) {
+    var urls = [
+        'https://plus.google.com/hangouts/*',
+        'https://talkgadget.google.com/hangouts/*'
+    ];
+
+    chrome.tabs.query({ url: urls }, function(tabs) {
         for (var i = 0; i < tabs.length; i++) {
             var tab = tabs[i];
 
-            chrome.tabs.sendMessage(tab.id, 'toggle');
+            chrome.tabs.sendMessage(tab.id, command);
         }
     });
 });
--- assets/js/update.js
+++ assets/js/update.js
@@ -1,33 +1,46 @@
 var muted = false;
+var mutedMicrophone = false;
+var mutedWebcam = false;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request == 'toggle') {
         muted = !muted;
 
-        updateButton('.ha-w-P-y-Xi-f'); // Microphone
-        updateButton('.ha-w-P-y-si-f'); // Webcam
+        updateButton('.gbbhzb', muted); // Microphone
+        updateButton('.YczAdf', muted); // Webcam
+
+        mutedMicrophone = muted;
+        mutedWebcam = muted;
 
         return true;
+    } else if (request == 'toggleMicrophone') {
+        mutedMicrophone = !mutedMicrophone;
+
+        updateButton('.gbbhzb', mutedMicrophone); // Microphone
+    } else if (request == 'toggleWebcam') {
+        mutedWebcam = !mutedWebcam;
+
+        updateButton('.YczAdf', mutedWebcam); // Webcam
     }
 
     return false;
 });
 
-function updateButton(selector) {
+function updateButton(selector, state) {
     // If muted == true AND hangouts muted == true, do nothing.
     // If muted == false AND hangouts muted == false, do nothing.
 
     var button = document.querySelector(selector);
 
     // If muted == false AND hangouts muted == true, simulate click in Hangouts.
-    if (button.parentElement.getAttribute('aria-pressed') == 'false' && muted) {
+    if (!button.classList.contains('U8OAre') && state) {
         simulateClick(button);
     }
 
     button = document.querySelector(selector); //Reselect to ensure there is a element which is valid, since we may have clicked simulateClick.
 
     // If muted == true AND hangouts muted == false, simulate click in Hangouts.
-    if (button.parentElement.getAttribute('aria-pressed') == 'true' && !muted) {
+    if (button.classList.contains('U8OAre') && !state) {
         simulateClick(button);
     }
 }
